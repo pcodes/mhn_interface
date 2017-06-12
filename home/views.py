9b13e95538fab96f3ff859421e497d105b24221d
Pat Cody <patcody028@gmail.com>
2017-06-12 12:47:03 -0400

    Connected index template to view

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Testing testing")
+    context = {}
+
+    return render(request, 'pages/home.html', context)
 
 # Create your views here.
