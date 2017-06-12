b075fca688c269abbba5b3c3fa85a320943a42b1
Pat Cody <patcody028@gmail.com>
2017-06-11 23:24:52 -0400

    Registered models with admin site, and added more useful identifier to Honeypot model

--- honeypots/models.py
+++ honeypots/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Honeypot(models.Model):
     team_id = models.IntegerField(default=-1)
     honey_id = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.honey_id
