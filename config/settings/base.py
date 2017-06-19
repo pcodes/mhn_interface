3f785b3c0b7bb0cda4da34346e945ae93fbbdce8
Pat Cody <patcody028@gmail.com>
2017-06-19 12:22:27 -0400

    Updated home URLs

--- config/settings/base.py
+++ config/settings/base.py
@@ -259,7 +259,7 @@ SOCIALACCOUNT_ADAPTER = 'mhn_interface.users.adapters.SocialAccountAdapter'
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/home/'
 LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
