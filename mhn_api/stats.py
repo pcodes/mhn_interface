9edcacbb3a8f3b17a88d9e914a6a591a2d903ad0
Pat Cody <patcody028@gmail.com>
2017-06-19 02:18:35 -0400

    Modified home page -Added top IPs and Ports when logged in -Added function to calculate top 5 items from list -Added prompt to tell user to log in on home page

--- mhn_api/stats.py
+++ mhn_api/stats.py
@@ -13,22 +13,33 @@ def get_request(params):
         api_key = {'api_key': env('MHN_API_KEY')}
 
     base_url = 'http://54.162.74.219/api/session'
-
-    request = {**api_key, **params}
+    if not params:
+        request = api_key
+    else:
+        request = {**api_key, **params}
 
     response = requests.get(base_url, params=request).json()
     return response
+
 
 def get_past_time_attacks(time):
     time_frame = {'hours_ago' : str(time)}
     past_attacks = get_request(time_frame)
     return len(past_attacks['data'])
 
+
+def get_user_time_attacks(time, honey_id):
+    time_frame = {'hours_ago' : str(time), 'identifier': honey_id}
+    past_attacks = get_request(time_frame)
+    return len(past_attacks['data'])
+
+
 def get_user_attacks(honeypot_id):
     id_param = {'identifier': honeypot_id}
     user_attacks = get_request(id_param)
     output = reverse_list(user_attacks['data'])
     return output
+
 
 def reverse_list(input_list):
     output = []
---
+++ mhn_api/util.py
@@ -0,0 +1,21 @@
+import operator
+
+
+def get_frequency(list, key):
+    total = {}
+    for x in list:
+        ip = x[key]
+        if ip in total:
+            total[ip] += 1
+        else:
+            total[ip] = 1
+    return total
+
+
+def get_top_5(list, key):
+    total = get_frequency(list, key)
+    top_5 = dict(sorted(iter(total.items()), key=operator.itemgetter(1), reverse=True)[:5])
+    print(top_5)
+    print(sorted(top_5, key=top_5.get, reverse=True))
+    return sorted(top_5, key=top_5.get, reverse=True)
+
